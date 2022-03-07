4e0f1040619f454d99833e5c27b0018014418cad
Webme <guillaume.gackel@gmail.com>
2022-03-07 19:15:41 +0100

    sidebar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Imessage from './Imessage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Faisons un Chat Message</h1>
+      <Imessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import React from "react";
+import './Sidebar.css';
+import SearchIcon from "@material-ui/icons/Search";
+import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <Avatar />
+        <div className="sidebar_input">
+          <SearchIcon />
+          <input placeholder="Search" />
+        </div>
+
+        <IconButton variant="outlined" className="sideBar_inputButton">
+          <RateReviewOutlinedIcon />
+        </IconButton>
+      </div>
+	  
+      <div className="sidebar_chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
